123a5affd898cc19b8ca60b47b48850271831d6d
Jonthss <jonathas9000@gmail.com>
2025-09-13 15:39:13 -0300

    Refactor createUser and updateUser functions to remove unnecessary fields and streamline user data handling

--- controllers/users.js
+++ controllers/users.js
@@ -23,11 +23,10 @@ const getSingle = async (req, res) => {
 const createUser = async (req, res) => {
     //#swagger.tags=['Users']
     const user = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
         email: req.body.email,
-        favoriteColor: req.body.favoriteColor,
-        birthday: req.body.birthday
+        username: req.body.username,
+        name: req.body.name,
+        ipaddress: req.body.ipaddress
     };
     const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
     if (response.acknowledged) {
@@ -41,11 +40,10 @@ const updateUser = async (req, res) => {
     //#swagger.tags=['Users']
     const userId = new ObjectId(req.params.id);
     const user = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
         email: req.body.email,
-        favoriteColor: req.body.favoriteColor,
-        birthday: req.body.birthday
+        username: req.body.username,
+        name: req.body.name,
+        ipaddress: req.body.ipaddress
     };
     const response = await mongodb.getDatabase().db().collection('users').replaceOne({ _id: userId }, user);
     if (response.modifiedCount > 0) {
